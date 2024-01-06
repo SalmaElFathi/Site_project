a6515cfa098b3dd6c3920a76eb3743d131dc0f65
SalmaElFathi <153767038+SalmaElFathi@users.noreply.github.com>
2024-01-06 23:47:48 +0100

    Update rechercher.php

--- rechercher.php
+++ rechercher.php
@@ -14,25 +14,41 @@ include('gestion.php');
         <form  class="formulaire" action="rechercher.php" method="POST">
             <h1>chercher un produit </h1>
             <table >
-                <tr><td>le nom du produit: </td><td><input type="text" name="mc"></td></tr>
+                <tr><td>le nom du produit: </td><td><input type="text" name="nom_pr"></td></tr>
             </table>
             <br>
-            <input type="submit" vlaue="rechercher">&nbsp;&nbsp;
+            <input type="submit" vlaue="rechercher" name="rechercher">&nbsp;&nbsp;
             <input type="reset" value="annuler">
         </form>
 </center>
-<?php 
-    if(isset($_POST['']))
-        if(!empty($_Post['']))
-        {$sql="select*from produit where name='".$_POST['name']."'";
-            $reponse=$bdd->query($sql);}
-        ?>
-            <center>
-                
-            </center>
-
-        
-    
- 
+<?php
+	if(isset($_POST['rechercher'])) 
+	{
+		if(!empty($_POST['nom_pr'])) 
+		{
+			
+			$query="select * from produit where nom='".$_POST['nom_pr']."'";
+			$reponse =mysqli_query($conn,$query);
+			$nbrDeLigne = mysqli_num_rows($reponse);
+			echo "<center> <b>Il y a ".$nbrDeLigne." Produit(s)</b></center>";
+?>
+			<center><table border="1">
+			    <tr bgcolor="#FF9966"><th>Catégorie</th><th>nom</th><th>marque</th><th>prix</th>
+			<?php
+				if($nbDeLigne>0)
+				{foreach($reponse as $row)
+				{echo "<tr><td>".$row['categorie']."</td>";
+				echo "<td>".$row['name']."</td>";
+				echo "<td>".$row['marque']."</td>";
+				echo "<td>".$row['prix']."</td>";
+				
+				echo "</tr>";
+				} 
+			echo "</table>";
+		}  }
+				else 
+				echo"<script>alert('Taper le nom du produit')</script>";
+	} 
+			?>
 </body>
 </html>
